c3b58471673abadf7ac4bd6343d706e0dfc93570
mualimkhair <mualimkhair@gmail.com>
2024-09-08 11:25:07 +0800

    first commit

---
+++ pertemuan1.js
@@ -0,0 +1,23 @@
+// no.1
+console.log("hello world!")
+
+// no.2
+let pemberitahuan = alert("selamat datang!")
+
+// no.3
+let nama = "laode"
+let umur = 19
+let apakahPelajar = true
+console.log(nama)
+console.log(umur)
+console.log(apakahPelajar)
+
+// no.4
+let a;
+let b;
+let c;
+
+a = 10
+b = 5
+c = a + b
+console.log(c)
